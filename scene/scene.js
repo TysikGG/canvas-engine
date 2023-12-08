96c13ded58f8301215787b5864d4a48560aeabe2
TysikGG <100859234+TysikGG@users.noreply.github.com>
2023-12-08 19:17:42 +0300

    1.1.1

--- scene/scene.js
+++ scene/scene.js
@@ -4,22 +4,7 @@ import { Sprite } from '../game/sprite.js'
 import { Animation } from '../game/animation.js';
 
 export let objects = [
-    new Rect(-1000, -300, 1000, 100, true, null, 0),
-    new Sprite("./animations/cat/1.png", {
-        sx: null,
-        sy: null,
-        sWidth: null,
-        sHeight: null,
-        dx: canvas1.width/2-200,
-        dy: canvas1.height/2-200,
-        dWidth: 300,
-        dHeight: 200,
-    }, {isStatic: true}, {
-        walk: new Animation("walk", "./animations/cat", 150, 4),
-    }, 1)
-]
-
-/*     new Rect(100, 100, 100, 100, false, {fill: true, color: '#00FF00'}, 1),
+    new Rect(100, 100, 100, 100, false, {fill: true, color: '#00FF00'}, 1),
     new Rect(canvas1.width/2 - 50 , canvas1.height/2 - 100, 100, 200, true, {fill: true, color: '#000000'}, 1),
     new Sprite("./animations/person/1.png", {
         sx: null,
@@ -46,4 +31,4 @@ export let objects = [
     }, {isStatic: false}, {
         idle: new Animation("idle", "./animations/cat", 150, 4),
     }, 1)
-*/
+]
